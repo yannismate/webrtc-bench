a9e0a2047c0fa3368635a65616918adaa89f3a86
Yannis Matezki <git@yannismate.de>
2025-09-01 20:46:10 +0200

    Update docker tag

--- docker-push.ps1
+++ docker-push.ps1
@@ -1,3 +1,3 @@
 docker push "docker.io/yannismate/webrtc-bench:latest"
-docker push "docker.io/yannismate/webrtc-bench-auto-receiver:v1.7"
-docker push "docker.io/yannismate/webrtc-bench-auto-sender:v1.7"
+docker push "docker.io/yannismate/webrtc-bench-auto-receiver:v1.8"
+docker push "docker.io/yannismate/webrtc-bench-auto-sender:v1.8"
